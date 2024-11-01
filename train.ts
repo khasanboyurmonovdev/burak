994c73e46fed09d7069dde2abbe4512929c852a5
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-02 01:45:58 +0900

    feat: H2-TASK is completed

--- train.ts
+++ train.ts
@@ -1,3 +1,25 @@
+/*H2-TASK: 
+
+Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+MASALAN: getDigits("m14i1t") return qiladi "141"
+
+@MITASK*/
+function getDigits(input: string): string {
+  let digits = "";
+
+  for (const char of input) {
+    if (/\d/.test(char)) {
+      digits += char;
+    }
+  }
+
+  return digits;
+}
+
+// Misol uchun funksiyani chaqirish
+const result = getDigits("m14i1tapqrstv879uuu888uuu");
+console.log(result);
+
 /*H-TASK: 
 
 shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
@@ -5,6 +27,7 @@ MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
 @MITASK
 */
+/*
 let str = getPositive([3, -6, 2, 15, -26, 38, -5, 5]);
 function getPositive(numbers: number[]): string {
   // Filter orqali faqat musbat sonlarni olish
@@ -16,7 +39,7 @@ function getPositive(numbers: number[]): string {
 console.log(typeof str);
 // Misol uchun:
 console.log(getPositive([3, -6, 2, 15, -26, 38, -5, 5]));
-
+*/
 /*G-TASK: 
 Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
 MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
