b825b411705330ab9c6d85a22dfc08d507720242
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-02 01:45:57 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 /*1- ENTRANCE*/
 const app = express(); // bu yerda expressni chaqirib olyapmiz
 console.log("__dirname", __dirname);
@@ -13,5 +14,6 @@ app.use(express.json()); // bu kod orqali rest API sifatida request bo'lyapkan d
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 /**4-ROUTERS */
-app.use("/", router); //Middleware design Pattern
+app.use("/admin", routerAdmin); //SSR: EJS
+app.use("/", router); //Middleware design Pattern //SINGLE PAGE APPLICATION: REACT
 export default app; // module.exports=app; bolardi common js da. bu yerda esma js da ishlatyapkanimiz uchun boshqacharo
