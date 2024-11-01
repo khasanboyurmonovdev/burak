1db4f1a12a8e09a3520744a0f1c667b2275e4b1d
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-02 01:46:23 +0900

    feat: develop verify authenticated user credentials

--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -4,7 +4,10 @@ import { Member } from "../libs/types/member";
 import jwt from "jsonwebtoken";
 
 class AuthService {
-  constructor() {}
+  private readonly secretToken;
+  constructor() {
+    this.secretToken = process.env.SECRET_TOKEN as string;
+  }
   public async createToken(payload: Member) {
     return new Promise((resolve, reject) => {
       const duration = `${AUTH_TIMER}h`;
@@ -21,6 +24,14 @@ class AuthService {
         }
       );
     });
+  }
+  public async checkAuth(token: string): Promise<Member> {
+    const result: Member = (await jwt.verify(
+      token,
+      this.secretToken
+    )) as Member;
+    console.log(`----[AUTH] membernick: ${result.memberNick}---`);
+    return result;
   }
 }
 export default AuthService;
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,12 @@
 import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
+/**Member */
+router.post("/member/login", memberController.login);
+router.post("/member/signup", memberController.signup);
+router.get("/member/detail", memberController.verifyAuth);
 
-router.post("/login", memberController.login);
-router.post("/signup", memberController.signup);
+/***Product */
 
+/**Order */
 export default router;
