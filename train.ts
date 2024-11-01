0eccb583987e1a42593fe7732542aa52aa6d83e0
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-02 01:45:57 +0900

    feat :H-TASK MIT IS DONE

--- train.ts
+++ train.ts
@@ -1,3 +1,22 @@
+/*H-TASK: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+@MITASK
+*/
+let str = getPositive([3, -6, 2, 15, -26, 38, -5, 5]);
+function getPositive(numbers: number[]): string {
+  // Filter orqali faqat musbat sonlarni olish
+  const positiveNumbers = numbers.filter((num) => num > 0);
+  // Musbat sonlarni string holatiga o'tkazish
+  const positiveString = positiveNumbers.join("");
+  return positiveString;
+}
+console.log(typeof str);
+// Misol uchun:
+console.log(getPositive([3, -6, 2, 15, -26, 38, -5, 5]));
+
 /*G-TASK: 
 Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
 MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
