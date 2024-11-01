2ab886b936081dac11fdb28b30c9432778288611
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-02 01:45:56 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,6 @@
 //MVC= MODEL VIEW CONTROLLER
 
 //Design pattern: Middleware, Decorator
+//import moment  from "moment";     //module js da bolsa require orniga import ni ishlatyapmiz  // const moment = require('moment'); common js da shunde ishlatganmiz
+import dotenv from "dotenv";
+dotenv.config(); // manashuyerda config metodini qolladik va shu yerda dotenv environmental variablelarni integratsiyasini amalga oshirdik
