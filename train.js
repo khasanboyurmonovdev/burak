914103d9ce669002afe8b2380712ee97e0c74560
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-02 01:45:55 +0900

    feat: G-TASK completed

---
+++ train.js
@@ -0,0 +1,17 @@
+/*G-TASK: 
+Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+@MITASK */
+
+function getHighestIndex(arr) {
+  let maxIndex = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[maxIndex]) {
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
+}
+
+// Misol:
+console.log(getHighestIndex([70, 80, 101, 1111, 999]));
